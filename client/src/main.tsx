087ec2b576d7a4c580d09178edd86baf0c9bcc0e
[dyad] <aymansamih383@gmail.com>
2026-03-02 00:30:09 +0000

    [dyad] Fixing QueryClientProvider error - wrote 2 file(s)

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,9 +3,13 @@ import App from "./App";
 import "./index.css";
 import "./lib/i18n";
 import { AuthProvider } from "./components/AuthProvider";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./lib/queryClient";
 
 createRoot(document.getElementById("root")!).render(
-  <AuthProvider>
-    <App />
-  </AuthProvider>
+  <QueryClientProvider client={queryClient}>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </QueryClientProvider>
 );
